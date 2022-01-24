539c26ecb04466fe115faa0e4087f9a620a88d54
Marina Gutkevich <gutmalina@mail.ru>
2022-01-24 14:05:04 +0300

    project4-review2

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 
-const profileOpenPopupButton = document.querySelector('.profile__edit') //переменна кнопки Редактировать
+const profileOpenPopupButton = document.querySelector('.button_type_edit-profil') //переменна кнопки Редактировать
 const popup = document.querySelector('.popup') //переменная формы Попап
-const popupCloseButton = document.querySelector('.popup__close') //переменная кнопки Закрыть
+const popupCloseButton = document.querySelector('.button_type_close-popup') //переменная кнопки Закрыть
 
 function openPopup() {
   popup.classList.add('popup_opened') // функция ОТКРЫТЬ ПОПАП
@@ -15,21 +15,29 @@ function closePopup() {
 profileOpenPopupButton.addEventListener('click', openPopup,) //клик по кнопке Редактировать
 popupCloseButton.addEventListener('click', closePopup) //клик по кнопке Закрыть
 
-const popupSaveButton = document.querySelector('.popup__save')//перменная кнопки Сохранить
-let NameInput = popup.querySelector('.popup__input_name') // переменна Имя в форме Попап
-let JobInput = popup.querySelector('.popup__input_job') // переменна работа в форме Попап
 
+let formElement = document.querySelector('.form') // переменная формы с именем и работой
+let NameInput = formElement.querySelector('.popup__input_edit_name') // переменна Имя в форме Попап
+let JobInput = formElement.querySelector('.popup__input_edit_job') // переменна работа в форме Попап
+const popupSaveButton = formElement.querySelector('.button_type_save-popup')//перменная кнопки Сохранить
 let NameProfile = document.querySelector('.profile__info-title') //переменная Текст имени
 let JobProfile = document.querySelector('.profile__info-subtitle') // переменная Текст работа
 
 NameInput.getAttribute('value')//получить значение имени из формы попап
 JobInput.getAttribute('value')//получить значение работа из формы попап
 
+popupSaveButton.addEventListener('click', formSubmitHandler) //клик по кнопке Сохранить - закроет форму попап и сохранит текст в профиль и отправит данные
 
-function savePopup () {//функция изменения текста в профиле и закрыть попап
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {//функция изменения текста в профиле и закрыть попап и отправить данные
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.Так мы можем определить свою логику отправки. О том, как это делать, расскажем позже.
   NameProfile.textContent = NameInput.value;//вставить значения из попап в текст профиля
   JobProfile.textContent = JobInput.value;//вставить значения из попап в текст профиля
-  popup.classList.remove('popup_opened') // функция ЗАКРЫТЬ ПОПАП  
+  closePopup() // функция ЗАКРЫТЬ ПОПАП
 }
 
- popupSaveButton.addEventListener('click', savePopup)//клик по кнопке Сохранить - закроет форму попап и сохранит текст в профиль
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
